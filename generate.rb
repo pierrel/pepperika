4aa2146b88037a594fba22fd4c305d5585e812a3
Pierre Larochelle <plarochelle@blurb.com>
2017-01-08 16:52:49 -0800

    30% faster with rx

--- generate.rb
+++ generate.rb
@@ -1,11 +1,24 @@
 require 'yaml'
+require 'rx'
 
 require './lib/parser'
 require './lib/utils'
 require './lib/transfer'
 require './lib/formatter'
 
-def page_to_hash(html)
+# get the cookie from the cookie file
+cookie = File.open('cookie.txt', 'r') {|file| file.read}.strip
+
+# grab the data and write it to a hash
+transfer = Transfer.new(cookie)
+
+Rx::Observable.of_enumerable(transfer.pages).take(50).map do |url|
+  # get the html for each url
+  sleep 0.01
+  puts "doing #{url}"
+  [url, transfer.page(url)]
+end.map do |url, html|
+  # parse the html
   parser = Parser.new(html)
 
   attrs = {
@@ -16,29 +29,39 @@ def page_to_hash(html)
   }
 
   img = parser.photo_url
-  attrs['photo'] = Utils.url_to_string(img) if img
+  attrs['photo_url'] = img
 
   src = parser.source_url
   attrs['source_url'] = src if src
 
-  Formatter.formatted(attrs)
-end
+  attrs
+end.map do |first_pass|
+  # convert url to image
+  #  first copy everything besides the url
+  attrs = {}
+  first_pass.each_pair do |key, val|
+    attrs[key] = val unless key == 'photo_url'
+  end
 
-# get the cookie from the cookie file
-cookie = File.open('cookie.txt', 'r') {|file| file.read}.strip
+  #  then download the image
+  img = first_pass['photo_url']
+  attrs['photo'] = Utils.url_to_string(img) if img
 
-# grab the data and write it to a hash
-transfer = Transfer.new(cookie)
-  
-data_hash = transfer.pages.map do |url|
-  sleep 0.01
-  puts "doing #{url}"
-  page = transfer.page(url)
-  
-  page_to_hash(page)
-end
-
-# open the yml file and write
-File.open("recipes.yml", "w") do |file|
-  file.write(data_hash.to_yaml)
-end
+  attrs
+end.map do |second_pass|
+  Formatter.formatted(second_pass)
+end.reduce([]) do |recipes, recipe|
+  recipes + [recipe]
+end.subscribe(
+  lambda do |final_arr|
+    File.open('recipes.yml', 'w') do |file|
+      file.write(final_arr.to_yaml)
+    end
+  end,
+  lambda do |error|
+    puts "error #{error}"
+  end,
+  lambda do
+    puts "done"
+  end
+)
